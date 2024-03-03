0cd496b9ba175e23fd0d2dfc96bd67720771ae54
praneelpa <153566595+praneelpa@users.noreply.github.com>
2024-03-02 18:37:05 -0800

    Update quizgame.py

--- Quiz Game - Beginner/quizgame.py
+++ Quiz Game - Beginner/quizgame.py
@@ -1,4 +1,3 @@
 import cli_box
 
-welcome = print(cli_box.rounded("Welcome to my quiz game! This game will quiz you on 10 basic 
-"general knowledge questions"))
+welcome = print(cli_box.rounded("Welcome to my quiz game! This game will quiz you on 10 basic general knowledge questions"))
