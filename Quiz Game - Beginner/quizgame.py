1620d9834b59347b03abe797c63a162e62b899f9
thenewcoder2969 <praneelpatel0@gmail.com>
2024-03-10 18:10:32 -0700

    Completed

--- Quiz Game - Beginner/quizgame.py
+++ Quiz Game - Beginner/quizgame.py
@@ -1,12 +1,13 @@
 import cli_box
 import sys
+from functions import game
 
-affirmative_responses = ['yes', 'y', 'why not', 'sure', 'i do']
+affirmative_responses = ['yes', 'y', 'why not', 'sure', 'i do', 'ya']
 negative_responses = ['no', 'n', 'nope', 'nah', 'i dont', 'i do not', "i don't"]
 while True:
     welcome = input("Welcome to my quiz game! This game will quiz you on 10 basic general knowledge questions. Do you want to play? ")
     if welcome.lower() in affirmative_responses:
-        ...
+        game()
     elif welcome.lower() in negative_responses:
         print(cli_box.rounded("Maybe next time! Goodbye!"))
         sys.exit()
